7d15dbcc728011d37787f489cc002e70d4e1e03a
Sâm <lethisam2003@gmail.com>
2026-06-29 22:40:18 +0700

    first commit

---
+++ src/test/java/Selenium/Topic01Enviorment.java
@@ -0,0 +1,26 @@
+package Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class Topic01Enviorment {
+    WebDriver driver;
+
+
+    @Test
+    public void TC_02_Run_On_Chrome() {
+        driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+
+    @Test
+    public void TC_03_Run_On_Edge() {
+        driver = new EdgeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+
+}
